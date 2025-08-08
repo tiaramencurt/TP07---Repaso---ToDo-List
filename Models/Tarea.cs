27e8b1cceab9723d0574a727324ff1c8f7456994
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2025-08-08 13:57:09 -0300

    mmm

---
+++ Models/Tarea.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace TP07.Models;
+public class Tarea
+{
+    [JsonProperty]
+    public int Id {get; private set;}
+    [JsonProperty]
+    public string Titulo {get; private set;}
+    [JsonProperty]
+    public string Descripcion {get; private set;}
+    [JsonProperty]
+    public DateTime Fecha {get; private set;}
+    [JsonProperty]
+    public bool Finalizada {get; private set;}
+    [JsonProperty]
+    public bool Eliminada {get; private set;}
+    [JsonProperty]
+    public int IdUsuario {get; private set;}
+    public Tarea()
+    {
+    }
+    public Tarea(string titulo, string descripcion, DateTime fecha, )
+    {
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace TP07.Models;
+public class Usuario
+{
+    [JsonProperty]
+    public int Id {get; private set;}
+    [JsonProperty]
+    public string Username {get; private set;}
+    [JsonProperty]
+    public string Password {get; private set;}
+    [JsonProperty]
+    public string Nombre {get; private set;}
+    [JsonProperty]
+    public string Apellido {get; private set;}
+    [JsonProperty]
+    public string Foto {get; private set;}
+    [JsonProperty]
+    public DateTime UltimoLogin {get; private set;}
+    public Usuario()
+    {
+    }
+    public Usuario(string username, string password, string nombre, string apellido, string foto)
+    {
+        this.Username = username;
+        this.Password = password;
+        this.Nombre = nombre;
+        this.Apellido = apellido;
+        this.Foto = foto;
+        this.UltimoLogin = null;
+    }
+}
