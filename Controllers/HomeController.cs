cbb7ce61133b878a09a2a6b3557fc2d571dbd9b4
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2025-08-08 15:55:36 -0300

    falta mucho

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        return RedirectToAction("Login", "Account");
     }
 
     public IActionResult CargarTareas(int idUsuario)
